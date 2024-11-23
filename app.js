fc1d0e7dad9cd2b7f671b118f752a43ae0c6bf21
Laxmi Narayan Pandey <l.n.p14112002@gmail.com>
2024-11-23 13:17:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,24 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyAvcovuNoRZmVPs7Ms9bNbqqzDoSrRYO80",
+  authDomain: "chitchat-1199e.firebaseapp.com",
+  projectId: "chitchat-1199e",
+  storageBucket: "chitchat-1199e.firebasestorage.app",
+  messagingSenderId: "26054136546",
+  appId: "1:26054136546:web:e376c20f53ebe4dcc244bb",
+  measurementId: "G-JCV9XXHG2X"
 };
 
 firebase.initializeApp(firebaseConfig);
